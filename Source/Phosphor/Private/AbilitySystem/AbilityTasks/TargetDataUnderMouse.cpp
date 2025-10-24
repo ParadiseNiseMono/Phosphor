166466cacdb11091cf7923f548e5538269e64020
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-10-24 11:25:29 +0800

    Send Mouse Cursor Data

--- Source/Phosphor/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Phosphor/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+#include "AbilitySystemComponent.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -12,8 +13,41 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+
+	const bool bIsLocallyControlled=Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)
+	{
+		SendMouseCursorData();
+	}
+	else
+	{
+		//TODO:We are on the Server,so listen for target data
+	}
+	
+	
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow ScopePredictionWindow(AbilitySystemComponent.Get());	
+	
 	APlayerController* PC=Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult Hit;
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, Hit);
-	ValidData.Broadcast(Hit.Location);
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data=new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult=Hit;
+	DataHandle.Add(Data);
+	
+
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(),
+		DataHandle,FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey);
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
