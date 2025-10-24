f615e9fc4bfc4a6fcd71d77586aa41e1eca29dfb
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-10-24 22:27:26 +0800

    Curve Tables

---
+++ Source/Phosphor/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// copyright Paradise_NiseMono
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
---
+++ Source/Phosphor/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,47 @@
+// copyright Paradise_NiseMono
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elemantalist,
+	Warrior,
+	Ranger
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly,Category="Class Default")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PHOSPHOR_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Character Class Default")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;	
+	
+	UPROPERTY(EditDefaultsOnly,Category="Common Class Default")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly,Category="Common Class Default")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+};
