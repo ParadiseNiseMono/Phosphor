318d5ad8a36c410aee430b5473b3da7950ba33d7
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-11-16 13:48:06 +0800

    First Trace Target

--- Source/Phosphor/Private/AbilitySystem/Abilities/PhosphorBeamSpell.cpp
+++ Source/Phosphor/Private/AbilitySystem/Abilities/PhosphorBeamSpell.cpp
@@ -4,6 +4,9 @@
 #include "AbilitySystem/Abilities/PhosphorBeamSpell.h"
 
 #include "GameFramework/Character.h"
+#include "Interaction/CombatInterface.h"
+#include "Kismet/KismetArrayLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UPhosphorBeamSpell::StoreMouseDataInfo(const FHitResult& MouseHitResult)
 {
@@ -24,5 +27,35 @@ void UPhosphorBeamSpell::StoreOwnerVariables()
 	{
 		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
+	}
+}
+
+void UPhosphorBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+	check(OwnerCharacter);
+	if (OwnerCharacter->Implements<UCombatInterface>())
+	{
+		if (USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+		{
+			TArray<AActor*> ActorsToIgnore;
+			ActorsToIgnore.Add(OwnerCharacter);
+			FHitResult HitResult;
+			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+			UKismetSystemLibrary::SphereTraceSingle(OwnerCharacter,
+				SocketLocation,
+				BeamTargetLocation,
+				10.f,
+				TraceTypeQuery1,
+				false,
+				ActorsToIgnore,
+				EDrawDebugTrace::None,
+				HitResult,
+				true);
+			if (HitResult.bBlockingHit)
+			{
+				MouseHitLocation = HitResult.ImpactPoint;
+				MouseHitActor = HitResult.GetActor();
+			}
+		}
 	}
 }
