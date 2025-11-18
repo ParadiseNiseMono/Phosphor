aa7040ca029cd5d7e6b3ddf0429567cece114576
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2025-11-18 20:59:30 +0800

    Magic Circle

---
+++ Source/Phosphor/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,27 @@
+// copyright Paradise_NiseMono
+
+
+#include "Actor/MagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicDecal");
+	MagicCircleDecal->SetupAttachment(RootComponent);
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Phosphor/Public/Actor/MagicCircle.h
@@ -0,0 +1,24 @@
+// copyright Paradise_NiseMono
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class PHOSPHOR_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AMagicCircle();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+
+};
